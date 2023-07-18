1b392086ea170e5a3562c663db98ae86489c6c43
Bewaaa <bethelehemtibebu19@gmail.com>
2023-07-18 16:27:13 -0700

    commit commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
+/**
+ * print_sign - prints the sign of a number
+ *@n: number being tested
+ * Return: Always 0.
+ */
 
-	
-	/**
-	 * main - check the code.
-	 *
-	 * Return: Always 0.
-	 */
-
-	int print_sign(int n)
+int print_sign(int n)
+{
+	if (n > 0)
 	{
-		if (n > 0)
-		{
-			_putchar('+');
-			return (1);
-		} else if (n == 0)
-		{
-			_putchar(48);
-			return (0);
-		} else if (n < 0)
-		{
-			_putchar('-');
-		}
-			return (-1);
+		_putchar('+');
+		return (1);
 	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
